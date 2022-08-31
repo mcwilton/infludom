927f47d1abdc7c2d62c8c9c13560f4aab83054e5
McWilton <mcwilton85@gmail.com>
2022-08-31 04:00:52 +0400

    heroku debugging

--- infludom/settings.py
+++ infludom/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-# import django_on_heroku
+import django_on_heroku
 from infludom.django_secret_key import key_generator
 import  environ
 
